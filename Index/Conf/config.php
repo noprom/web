4b917a51134b834287961c016e73ad2b107cd503
noprom <874702417@qq.com>
2014-11-04 20:54:48 +0800

    微博Clone
    微博Clone

---
+++ Index/Conf/config.php
@@ -0,0 +1,49 @@
+<?php
+return array(
+	//数据库配置
+	'DB_HOST' => '127.0.0.1',	//数据库服务器地址
+	'DB_USER' => 'root',	//数据库连接用户名
+	'DB_PWD' => '',	//数据库连接密码
+	'DB_NAME' => 'weibo', //使用数据库名称
+	'DB_PREFIX' => 'hd_',	//数据库表前缀
+
+	'DEFAULT_THEME' => 'default',	//默认主题模版
+
+	'URL_MODEL' => 2,
+
+	'TOKEN_ON' => false,	//关闭令牌功能
+
+	//用于异位或加密的KEY
+	'ENCTYPTION_KEY' => 'www.houdunwang.com',
+	//自动登录保存时间
+	'AUTO_LOGIN_TIME' => time() + 3600 * 24 * 7,	//一个星期
+
+	//图片上传
+	'UPLOAD_MAX_SIZE' => 2000000,	//最大上传大小
+	'UPLOAD_PATH' => './Uploads/',	//文件上传保存路径
+	'UPLOAD_EXTS' => array('jpg', 'jpeg', 'gif', 'png'),	//允许上传文件的后缀
+
+	//URL路由配置
+	'URL_ROUTER_ON' => true,	//开启路由功能
+	'URL_ROUTE_RULES' => array(	//定义路由规则
+		':id\d' => 'User/index',
+		'follow/:uid\d' => array('User/followList', 'type=1'),
+		'fans/:uid\d' => array('User/followList', 'type=0'),
+		),
+
+	//自定义标签配置
+	'TAGLIB_LOAD' => true,	//加载自定义标签库
+	'APP_AUTOLOAD_PATH' => '@.TagLib',	//自动加载
+	'TAGLIB_BUILD_IN' => 'Cx,Hdtags',	//加入系统标签库
+
+	//缓存设置
+	// 'DATA_CACHE_SUBDIR' => true,	//开启以哈希形式生成缓存目录
+	// 'DATA_PATH_LEVEL' => 2,	//目录层次
+	'DATA_CACHE_TYPE' => 'Memcache',
+	'MEMCACHE_HOST' => '127.0.0.1',
+	'MEMCACHE_PORT' => 11211,
+
+	//加载扩展配置
+	'LOAD_EXT_CONFIG' => 'system',
+);
+?>
---
+++ Index/Lib/Action/SearchAction.class.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * 搜索控制器
+ */
+Class SearchAction extends CommonAction {
+
+	/**
+	 * 搜索找人
+	 */
+	Public function sechUser () {
+		$keyword = $this->_getKeyword();//取得关键字
+		
+		if ($keyword) {
+			//检索出除自己外呢称含有关键字的用户
+			$where = array(
+				'username' => array('LIKE', '%' . $keyword . '%'),
+				'uid' => array('NEQ', session('uid'))
+				);
+			$field = array('username', 'sex', 'location', 'intro', 'face80', 'follow', 'fans', 'weibo', 'uid');
+
+			$db = M('userinfo');
+
+			//导入分页类
+			import('ORG.Util.Page');
+			$count = $db->where($where)->count('id');
+			$page = new Page($count, 20);
+			$limit = $page->firstRow . ',' . $page->listRows;
+			$result = $db->where($where)->field($field)->limit($limit)->select();
+			
+			//重新组合结果集，得到是否已关注与是否互相关注
+			$result = $this->_getMutual($result);
+
+			//分置搜索结果到视图
+			$this->result = $result ? $result : false;
+			//页码
+			$this->page = $page->show();
+		}
+
+		$this->keyword = $keyword;
+		$this->display();
+	}
+
+	/**
+	 * 搜索微博
+	 */
+	Public function sechWeibo () {
+		$keyword = $this->_getKeyword();
+		
+		if ($keyword) {
+			//检索含有关键字的微博
+			$where = array('content' => array('LIKE', '%' . $keyword . '%'));
+
+			$db = D('WeiboView');
+
+			//导入分页类
+			import('ORG.Util.Page');
+			$count = M('weibo')->where($where)->count('id');
+			$page = new Page($count, 20);
+			$limit = $page->firstRow . ',' . $page->listRows;
+			$weibo = $db->getAll($where, $limit);
+
+			$this->weibo = $weibo ? $weibo : false;
+			//页码
+			$this->page = $page->show();
+		}
+
+		$this->keyword = $keyword;
+		$this->display();
+	}
+
+	/**
+	 * 返回搜索关键字
+	 */
+	Private function _getKeyword () {
+		return $_GET['keyword'] == '搜索微博、找人' ? NULL : $this->_get('keyword');
+	}
+
+	/**
+	 * 重组结果集得到是否互相关注与是否已关注
+	 * @param  [Array] $result [需要处理的结果集]
+	 * @return [Array]         [处理完成后的结果集]
+	 */
+	Private function _getMutual ($result) {
+		if (!$result) return false;
+
+		$db = M('follow');
+
+		foreach ($result as $k => $v) {
+			//是否互相关注
+			$sql = '(SELECT `follow` FROM `hd_follow` WHERE `follow` = ' . $v['uid'] . ' AND `fans` = ' . session('uid') . ') UNION (SELECT `follow` FROM `hd_follow` WHERE `follow` = ' . session('uid') . ' AND `fans` = ' . $v['uid'] . ')';
+			$mutual = $db->query($sql);
+			
+			if (count($mutual) == 2) {
+				$result[$k]['mutual'] = 1;
+				$result[$k]['followed'] = 1;
+			} else {
+				$result[$k]['mutual'] = 0;
+
+				//未互相关注是检索是否已关注
+				$where = array(
+					'follow' => $v['uid'],
+					'fans' => session('uid')
+					);
+				$result[$k]['followed'] = $db->where($where)->count();
+			}
+		}
+		return $result;
+	}
+}
+?>
